123e0d3d4d1d97aa68fbf18eba153b9112a60b3e
nareshix <nareshix66@gmail.com>
2026-09-12 18:14:30 +0800

    unused code

--- sqlitex_core/src/internal_sqlite/mod.rs
+++ sqlitex_core/src/internal_sqlite/mod.rs
@@ -2,4 +2,3 @@ pub mod dynamic_rows;
 pub mod preparred_statement;
 pub mod rows_dao;
 pub mod sqlitex_connection;
-pub mod sqlitex_statement;
--- sqlitex_core/src/internal_sqlite/sqlitex_statement.rs
+++
@@ -1,8 +0,0 @@
-#[allow(unused)]
-pub struct SqlitexStmt {
-    pub sql_query: &'static str,
-}
-
-// sqlite default mode is serialized
-unsafe impl Send for SqlitexStmt {}
-unsafe impl Sync for SqlitexStmt {}
